23df1df3dadffa1ab128fe522ce26c62ab9ae0b5
Viktor Khomyn <viktor.khomyn@sombrainc.com>
2023-04-11 00:42:03 +0300

    Implemented AuthController Fixed issue with appsetting.json file

--- CourseManagementBackOffice/Controllers/AuthController.cs
+++ CourseManagementBackOffice/Controllers/AuthController.cs
@@ -4,10 +4,31 @@
     [ApiController]
     public class AuthController : ControllerBase
     {
-        [HttpPost("register")]
-        public ActionResult<User> Register(UserDto request)
-        {
+        private readonly IAuthService _authService;
 
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserDto request)
+        {
+            switch (_authService.Register(request))
+            {
+                case RegistrationStatus.Success:
+                    return Ok();
+                default:
+                    return BadRequest("User with this E-mail already exists");
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(UserDto request)
+        {
+            var token = _authService.Login(request);
+            
+            return !string.IsNullOrEmpty(token) ? Ok(token) : BadRequest("Invalid credentials");
         }
     }
 }
